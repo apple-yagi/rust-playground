c8d585f0f0392c4ae8b8eea39cb30cf4a0e02ced
apple-yagi <applesryu@gmail.com>
2022-05-15 02:12:53 +0900

    Impl gzip compress

--- gzip-compress/src/main.rs
+++ gzip-compress/src/main.rs
@@ -1,3 +1,28 @@
+use flate2::write::GzEncoder;
+use flate2::Compression;
+use std::fs;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let filename = "Cargo.toml";
+
+    // Open file
+    let mut file = fs::File::open(filename).expect("Failed to open file");
+
+    // Print file size
+    println!("{}", file.metadata().unwrap().len());
+
+    // Read file
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("Failed to read file");
+
+    // Gzip encode
+    let output = Vec::new();
+    let mut encoder = GzEncoder::new(output, Compression::default());
+    encoder
+        .write_all(contents.as_bytes())
+        .expect("Error: write_all(contents.as_bytes())");
+    let output = encoder.finish().unwrap();
+    println!("{:?}", output.len());
 }
